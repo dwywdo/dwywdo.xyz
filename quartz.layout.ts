97c1fa7919a748888c3f9e03f05f76c95b82c404
Euiyub Jung <euiyub.jung@linecorp.com>
2025-07-10 02:41:10 +0900

    Quartz sync: Jul 10, 2025, 2:41 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,23 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        // from data-repo
+        repo: 'dwywdo/blog.dwywdo.dev',
+        // from data-repo-id
+        repoId: 'R_kgDOPJjWjw',
+        // from data-category
+        category: 'Announcements',
+        // from data-category-id
+        categoryId: 'DIC_kwDOPJjWj84Cstsh',
+        // from data-lang
+        lang: 'en'
+      }
+    }),
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
