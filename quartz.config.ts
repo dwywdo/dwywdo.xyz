6188e24220ecb1449c1baec5d49f77812dce0aae
Euiyub Jung <euiyub.jung@linecorp.com>
2025-07-10 14:31:10 +0900

    Quartz sync: Jul 10, 2025, 2:31 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "DM Serif Display",
-        body: "Source Sans Pro",
+        body: "Bricolage Grotesque",
         code: "IBM Plex Mono",
       },
       colors: {
